16898bf989dea4200ef9a6933cb9a38258709813
Nahuel Alvarez <alvareznahue@gmail.com>
2024-05-11 15:01:54 -0300

    mejora de botones

--- index.js
+++ index.js
@@ -6,24 +6,28 @@ const btn4 = document.getElementById('btn-twister4');
 class Twister {
   constructor() {
     this.level = 1;
+    this.colors = ['green', 'red', 'blue', 'yellow'];
+    this.playingSequence = false;
     this.sequence = [];
     this.playerSequence = [];
-    this.colors = ['green', 'red', 'yellow', 'purple'];
-    this.playingSequence = false;
     btn1.addEventListener('click', () => this.handleButtonClick('green'));
     btn2.addEventListener('click', () => this.handleButtonClick('red'));
-    btn3.addEventListener('click', () => this.handleButtonClick('yellow'));
-    btn4.addEventListener('click', () => this.handleButtonClick('purple'));
+    btn3.addEventListener('click', () => this.handleButtonClick('blue'));
+    btn4.addEventListener('click', () => this.handleButtonClick('yellow'));
+    this.startGame();
   }
 
   startGame() {
-    this.generateSequence();
+    this.generateSequence(); // Genera la secuencia al inicio del juego
     this.showSequence();
+    console.log(this.sequence)
   }
 
   generateSequence() {
-    const randomColor = this.getRandomColor();
-    this.sequence.push(randomColor);
+    for (let i = 0; i < this.level; i++) {
+      const randomColor = this.getRandomColor();
+      this.sequence.push(randomColor);
+    }
   }
 
   handleButtonClick(color) {
@@ -37,7 +41,6 @@ class Twister {
 
   async showSequence() {
     this.playingSequence = true;
-
     for (let i = 0; i < this.sequence.length; i++) {
       const color = this.sequence[i];
       await this.lightUpButton(color);
@@ -45,7 +48,6 @@ class Twister {
       this.resetButtons();
       await this.sleep(500);
     }
-
     this.playingSequence = false;
   }
 
@@ -57,10 +59,10 @@ class Twister {
       case 'red':
         btn2.style.opacity = 1;
         break;
-      case 'yellow':
+      case 'blue':
         btn3.style.opacity = 1;
         break;
-      case 'purple':
+      case 'yellow':
         btn4.style.opacity = 1;
         break;
     }
@@ -80,34 +82,37 @@ class Twister {
   getPlayerInput(color) {
     if (!this.playingSequence) {
       this.lightUpButton(color); // Ilumina el botón al hacer clic
-
-      // Compara el color clickeado con el próximo color en la secuencia
       const expectedColor = this.sequence[this.playerSequence.length];
-
       if (color === expectedColor) {
-        // El jugador hizo clic en el color correcto
         this.playerSequence.push(color);
-
         if (this.playerSequence.length === this.sequence.length) {
-          // El jugador completó la secuencia actual
           this.levelUp();
         }
       } else {
-        // El jugador hizo clic en el color incorrecto
         alert('¡Seleccionaste un color incorrecto! El juego se reiniciará.');
         this.restartGame();
       }
+    } else {
+      if (this.playerSequence.length > 0) {
+        alert('Espera tu turno. Ahora está jugando el juego.');
+      }
     }
+    console.log(this.playerSequence);
   }
 
   levelUp() {
     alert('¡Nivel completado! Subiendo al siguiente nivel.');
     this.level++;
     this.playerSequence = [];
-    this.generateSequence();
-    this.showSequence();
-  }
-  
+    const newColor = this.getRandomColor(); // Generar un nuevo color para agregar a la secuencia
+    this.sequence.push(newColor); // Agregar el nuevo color a la secuencia existente
+    setTimeout(() => {
+      this.showSequence(); // Mostrar la nueva secuencia después de un breve retraso
+    }, 1000);
+    console.log(this.sequence)
+}
+
+
   restartGame() {
     alert('El juego se reinicia.');
     this.level = 1;
@@ -118,4 +123,3 @@ class Twister {
 }
 
 const twisterMemoGame = new Twister();
-twisterMemoGame.startGame();
